cfe3e28ad0f58fd953915595299ad5cb9dd767c3
kz1000a1 <146339266+kz1000a1@users.noreply.github.com>
2024-04-12 19:57:34 +0900

    Update mx5_nc_logger.ino

--- mx5_nc_logger.ino
+++ mx5_nc_logger.ino
@@ -184,7 +184,7 @@ void mazdaMx5Brake(twai_message_t* rx_frame) {
 
 void mazdaMx5OutputCsv() {
   // Serial.println("mazdaMx5OutputCsv()");
-  Serial.printf(", %3.1f, %4.1f, %d, %3.1f, %3.1f, %-2.1f\n", Speed, EngineRPM, ShiftPosition, AcceleratorPosition, BrakePercentage, SteeringAngle * MAX_STEERING_ANGLE / STEERING_MAX);
+  Serial.printf(", %.1f, %.1f, %d, %.1f, %.1f, %-.1f\n", Speed, EngineRPM, ShiftPosition, AcceleratorPosition, BrakePercentage, SteeringAngle * MAX_STEERING_ANGLE / STEERING_MAX);
 }
 
 void setup() {
