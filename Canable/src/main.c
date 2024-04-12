fcd7719642fe01974f8c87ee65d6bcebcf51961e
kz1000a1 <146339266+kz1000a1@users.noreply.github.com>
2024-04-13 07:19:45 +0900

    Update main.c

--- Canable/src/main.c
+++ Canable/src/main.c
@@ -33,7 +33,7 @@ int16_t bytesToInt(uint8_t raw[], int shift, int size) {
     // printf_("result=%04X << %d = ",result, sizeof(byte) * 8);
     result = result << (sizeof raw[0] * 8);
     // printf_("%d\n",result);
-    for (int j = 0; j < sizeof(byte) * 8; j++) {
+    for (int j = 0; j < sizeof raw[0] * 8; j++) {
       // printf_("%04X & %04X = %d += %04X,result=%04X\n", raw[i + shift], 1 << j,raw[i + shift] & (1 << j),result);
       // if(raw[i + shift] & (1 << j))(result += 1 << j);
       result += raw[i + shift] & (1 << j);
@@ -54,7 +54,7 @@ uint16_t bytesToUint(uint8_t raw[], int shift, int size) {
     // printf_("result=%04X << %d = ",result, sizeof(byte) * 8);
     result = result << (sizeof raw[0] * 8);
     // printf_("%d\n",result);
-    for (int j = 0; j < sizeof(byte) * 8; j++) {
+    for (int j = 0; j < sizeof raw[0] * 8; j++) {
       // printf_("%04X & %04X = %d += %04X,result=%04X\n", raw[i + shift], 1 << j,raw[i + shift] & (1 << j),result);
       // if(raw[i + shift] & (1 << j))(result += 1 << j);
       result += raw[i + shift] & (1 << j);
@@ -79,6 +79,21 @@ uint16_t bitsToUint(uint8_t raw[], int shift, int size) {
   return result;
 
   // return &raw[shift];
+}
+
+uint16_t bitToUint(uint8_t raw[], int shift) {
+  uint16_t result;
+  // Serial.printf("raw[%d]=%02X,>> %d = 0x%02X, & 0xFE\n",(shift - 1) / 8,raw[(shift - 1) / 8],7 - (shift - 1) % 8,(raw[(shift - 1) / 8] >> 7 - (shift - 1) % 8));
+
+  result = (raw[(shift - 1) / 8] >> 7 - (shift - 1) % 8) & 0x01;
+  /*
+  printf("result0=%02X, result1=",result);
+  result = result & 0x01;
+  printf("%02X\n",result);
+  } else {
+    result = (raw[(shift - 1) / 8] & 0xFE);
+  }*/
+  return result;
 }
 
 void mazdaMx5EngineSpeed(uint8_t* rx_msg_data) {
